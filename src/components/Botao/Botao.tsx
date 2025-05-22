05d748395720e075e980f19cb379b220d5be80b7
AdmAlexandreMattos <admalexandremattos@gmail.com>
2025-05-22 16:40:22 -0300

    Estilizando com Sass
    Instalando o Sass e modificando o css do projeto por completo.

--- src/components/Botao/Botao.tsx
+++ src/components/Botao/Botao.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./Botao.css";
+import "./Botao.scss";
 
 export class Botao extends React.Component {
   render() {
-    return <button>Botão</button>;
+    return <button className="botao">Botão</button>;
   }
 }
--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,4 +1,4 @@
-import "./Lista.css";
+import "./Lista.scss";
 
 export function Lista() {
   const tarefas = [
@@ -16,11 +16,11 @@ export function Lista() {
     },
   ];
   return (
-    <aside>
+    <aside className="listaTarefas">
       <h2> Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index}>
+          <li key={index} className="item">
             <h3>{item.tarefa}</h3>
             <span>{item.tempo}</span>
           </li>
