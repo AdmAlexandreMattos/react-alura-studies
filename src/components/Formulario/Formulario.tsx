0a765afb3b12c25cddaef90982918e86e2f1b686
AdmAlexandreMattos <admalexandremattos@gmail.com>
2025-05-23 15:10:18 -0300

    Controlando o formulário
    Refatorando o formulário

--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -3,15 +3,32 @@ import Botao from "../Botao";
 import style from "./Formulario.module.scss";
 
 export class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log("Teste");
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form
+        className={style.novaTarefa}
+        onSubmit={this.adicionarTarefa.bind(this)}
+      >
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={(evento) =>
+              this.setState({ ...this.state, tarefa: evento.target.value })
+            }
             placeholder="O que você quer estudar?"
             required
           />
@@ -22,6 +39,10 @@ export class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={(evento) =>
+              this.setState({ ...this.state, tempo: evento.target.value })
+            }
             id="tempo"
             min="00:00:00"
             max="01:30:00"
